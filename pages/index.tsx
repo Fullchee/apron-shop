d72a1fa94c545910d012bea2afef4bfb58dd95c2
Fullchee Zhang <fullchee@gmail.com>
2022-09-22 07:11:36 -0400

    Get one of each product, banner and category

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,9 +30,40 @@ const Home = ({
 export default Home;
 
 export async function getStaticProps() {
-  const banners = [];
-  const categories = [];
-  const products = [];
+  const banners = [
+    {
+      _id: '5e3e88581c9d440000d0f600',
+      name: 'women',
+      imageURL: 'https://res.cloudinary.com/djlbfjouc/image/upload/v1582518195/banner-2_htripb.jpg',
+    },
+  ];
+  const categories = [
+    {
+      _id: '5e3e897d1c9d440000d0f603',
+      imageURL: 'https://res.cloudinary.com/djlbfjouc/image/upload/v1582274252/categ-02_pqpnm7.jpg',
+      name: 'Men',
+    },
+    {
+      _id: '5e3e89e71c9d440000d0f604',
+      name: 'Women',
+      imageURL:
+        'https://res.cloudinary.com/djlbfjouc/image/upload/v1582274091/ezgif.com-webp-to-jpg_l9flc0.jpg',
+    },
+  ];
+  const products = [
+    {
+      _id: '5e3e8f19f4a19c0417b8dafd',
+      name: 'Long Sleeves Polka Dots',
+      price: 900,
+      description:
+        'Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.',
+      imageURL:
+        'https://res.cloudinary.com/djlbfjouc/image/upload/v1581158167/sbiuoziiqi5gkuvrsymv.jpg',
+      category: 'women',
+      createdAt: '2020-02-08T10:36:09.862Z',
+      updatedAt: '2020-02-08T10:36:09.862Z',
+    },
+  ];
 
   return {
     props: {
