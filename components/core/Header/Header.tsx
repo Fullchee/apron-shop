887df89d2f525f5f15c77124e6e97ffeb11e9748
Fullchee Zhang <fullchee@gmail.com>
2022-09-22 10:05:41 -0400

    Add icon

--- components/core/Header/Header.tsx
+++ components/core/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -12,7 +13,14 @@ const Header = () => {
       <header className={styles.header}>
         <ul className={styles.headerWrapper}>
           <Link href="/">
-            <a className={styles.siteTitle}>Leslieville Aprons</a>
+            <a className={styles.siteTitle}>
+              <Image
+                src="https://res.cloudinary.com/dnboci3v7/image/upload/v1663853715/leslieville-aprons/leslieville-apron-side-logo_z4ji1c.png"
+                alt="home page"
+                width={194}
+                height={75}
+              />
+            </a>
           </Link>
           <div className={styles.headerRight}>
             <DesktopMenu />
